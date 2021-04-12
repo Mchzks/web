141ee8ec959e86f14d3c11ecf1708f920875ebb8
Danielesk1 <art.copor@yahoo.com>
2021-04-12 08:36:13 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# WEB Daniel Villalobos
+My Official Website.
+<br><br>
+Creator: Daniel Villalobos.
+<br><br>
+Key Technologies used: HTML5 - CSS3 - JavaScript
+
---
+++ assets/js/main.js
@@ -0,0 +1,156 @@
+
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+/*Active link*/
+navLink.forEach(n => n.classList.remove('active'));
+this.classList.add('active');
+
+/*Remove menu mobile*/
+const navMenu = document.getElementById('nav-menu')
+navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{});
+sr.reveal('.button',{delay: 200});
+sr.reveal('.home__img',{delay: 400});
+sr.reveal('.home__social-icon',{ interval: 200});
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{});
+sr.reveal('.about__subtitle',{delay: 400});
+sr.reveal('.about__text',{delay: 400});
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{});
+sr.reveal('.skills__text',{});
+sr.reveal('.skills__data',{interval: 200});
+sr.reveal('.hab__img',{delay: 600});
+
+/*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200});
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 200});
+
+
+/*BACK TO TOP BUTTON*/
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) { // Show backToTopButton
+    if(!backToTopButton.classList.contains("btnEntrance")) {
+    backToTopButton.classList.remove("btnExit");
+    backToTopButton.classList.add("btnEntrance");
+    backToTopButton.style.display = "block";
+    }
+}
+  else { // Hide backToTopButton
+    if(backToTopButton.classList.contains("btnEntrance")) {
+    backToTopButton.classList.remove("btnEntrance");
+    backToTopButton.classList.add("btnExit");
+    setTimeout(function() {
+        backToTopButton.style.display = "none";
+    }, 250);
+    }
+}
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+function smoothScrollBackToTop() {
+const targetPosition = 0;
+const startPosition = window.pageYOffset;
+const distance = targetPosition - startPosition;
+const duration = 750;
+let start = null;
+
+window.requestAnimationFrame(step);
+
+function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+}
+}
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+	return c/2*(t*t*t + 2) + b;
+}
+
+/* BALLS ANIMATION */
+const colors = ["#38ff45", 
+                "#2c29ff", 
+                "#bb29ff", 
+                "#fff838", 
+                "#ff3838",
+                "#ff6929",
+                "#29ffdb"];
+const numBalls = 80;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * 0.1,
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000,
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
